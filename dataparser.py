2abd0703058e47e92b4b394a13cf3aaccd4ce8c3
Paul Ammann <meanmrclean@gmail.com>
2016-09-21 16:52:43 -0400

    modifying dataparser for generalized n-gram

--- dataparser.py
+++ dataparser.py
@@ -11,12 +11,22 @@ from random import randint
 # Classes
 #
 
+#
+# NGram recursively calls itself
+#
 class NGram:
-    def __init__(self):
-        self.counts = defaultdict(int)
+    def __init__(self, depth=1):
+        self.depth = depth
+        self.counts = defaultdict(NGram)
         self.total = 0
-    def add(self, word):
-        self.counts[word] +=1
+    def add(self, words):
+        # if self.depth != len(words) :
+        #     raise IndexError("Words must be an array of length equal to the depth of the nGram")
+        if self.depth == 1:
+            self.counts[words[0]].total +=1
+        else:
+            # continue adding the rest of the words through layers of the nGram
+            self.counts[words[0]].add(words[1:])
         self.total +=1
 #
 # Functions
@@ -45,21 +55,21 @@ def unigramGenSentence(unigrams, initial):
         initial += " "+ new_word
     return initial
 
-def findSeedWord(bigram, random_word_index):
-    for k,v in bigram.counts.iteritems():
-        random_word_index -= v
+def findSeedWord(ngram, random_word_index):
+    for k,v in ngram.counts.iteritems():
+        random_word_index -= v.total
         if random_word_index <= 0:
             return k
     return "</s>"
 
 def bigramGenSentence (bigrams, initial):
     seed_word = initial
-    seed_gram = bigrams[seed_word]
+    seed_gram = bigrams.counts[seed_word]
     while seed_gram.total > 0 and seed_word != "</s>":
         word_index = randint(0,seed_gram.total)
         seed_word = findSeedWord(seed_gram, word_index)
         initial += " "+ seed_word
-        seed_gram = bigrams[seed_word]
+        seed_gram = bigrams.counts[seed_word]
     return initial
 
 #
@@ -79,8 +89,8 @@ for category in category_paths:
     print("------------",category,"------------")
     print("---")
 
-    unigrams = NGram()
-    bigrams = defaultdict(NGram)
+    unigrams = NGram(1)
+    bigrams = NGram(2)
 
     file_paths = glob.glob(category+"/train_docs/*.txt")
     # file_paths = [file_paths[0]]
@@ -99,9 +109,9 @@ for category in category_paths:
                 tokenIdx_1 = "<s>"
                 if idx != 0:
                     tokenIdx_1 = tokens[idx-1]
-                unigrams.add(token)
 
-                bigrams[tokenIdx_1].add(token)
+                bigrams.add([tokenIdx_1,token])
+                unigrams.add([token])
 
 
     print("-------- Unigram ----------")
