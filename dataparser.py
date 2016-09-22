dbcd8ad97129ee6a0abb7b9cb68bf609e2645d9f
Paul Ammann <meanmrclean@gmail.com>
2016-09-22 13:12:47 -0400

    computing perplexity for bigrams

--- dataparser.py
+++ dataparser.py
@@ -6,6 +6,7 @@ from nltk import word_tokenize
 from nltk.tokenize import sent_tokenize
 from collections import defaultdict
 from random import randint
+import math
 
 #
 # Classes
@@ -19,6 +20,7 @@ class NGram:
         self.depth = depth
         self.counts = defaultdict(NGram)
         self.total = 0
+        self.smoothTotal = 0
     def add(self, words):
         # if self.depth != len(words) :
         #     raise IndexError("Words must be an array of length equal to the depth of the nGram")
@@ -47,6 +49,42 @@ def stringPreprocessor(string):
     regex = re.compile('(' + email_regex + '|' + chars + ')', re.VERBOSE)
     return re.sub(regex, '', string)
 
+#
+# Loops over files to read, for bigrams replaces count 1 tokens with <unk> via readToken
+def collectionIterator(file_paths,ngrams, unigrams=False):
+    for path in file_paths:
+        # print ("Now reading %s", path)
+        raw = open(path, 'r').read()
+        processed_file = filePreprocessor(raw)
+        sen_tokens = sent_tokenize(processed_file)
+        for sen in sen_tokens:
+            # might need to point </s> to <s> for bigrams
+
+            sentence = stringPreprocessor(sen)
+            tokens = word_tokenize(sentence)
+            tokens.append("</s>")
+            for idx, token in enumerate(tokens):
+                if unigrams:
+                    # doing bigram
+                    tokenIdx_1 = "<s>"
+                    if idx != 0:
+                        tokenIdx_1 = readToken(tokens[idx-1],unigrams)
+                    token = readToken(token, unigrams)
+                    ngrams.add([tokenIdx_1,token])
+                else:
+                    ngrams.add([token])
+    return ngrams;
+
+def readToken(token, unigrams):
+    if unigrams.counts[token].total > 1:
+        return token
+    return "<unk>"
+
+def readTestToken(token, unigrams):
+    if unigrams.counts[token].total > 0:
+        return token
+    return "<unk>"
+
 def unigramGenSentence(unigrams, initial):
     new_word = initial
     while new_word != "</s>":
@@ -73,6 +111,129 @@ def bigramGenSentence (bigrams, initial):
     return initial
 
 #
+# smoothing funcitons
+def removeUnk(unigrams):
+    deleteKeys = []
+    for k,v in unigrams.counts.iteritems():
+        if v.total == 1:
+            deleteKeys.append(k)
+    for k in deleteKeys:
+        unigrams.counts.pop(k,None)
+        unigrams.add(["<unk>"])
+    return unigrams
+
+def readCmap(count, cmap):
+    if count >= len(cmap):
+        return count;
+    else:
+        return cmap[count]
+
+def smoothGoodTuring(bigram):
+    vocab_size = len(bigram.counts)
+    # N0 = V^2 - N
+    Narray = [0]*12
+    Narray[0] = vocab_size * vocab_size - bigram.total
+    for k,v in bigram.counts.iteritems():
+        for k2, v2 in v.counts.iteritems():
+            if v2.total < 12:
+                Narray[v2.total] += v2.total
+
+    cmap = [0]*11
+    for c in range(11):
+        cmap[c] = (c+1)* Narray[c+1] / Narray[c]
+    print("and the cmap is ", cmap)
+
+    # compute adjusted totals
+    for k,v in bigram.counts.iteritems():
+        numNgrams = 0
+        smoothTotal = 0
+        for k2, v2 in v.counts.iteritems():
+            numNgrams += 1
+            v2.smoothTotal = readCmap(v2.total, cmap)
+            smoothTotal += v2.smoothTotal
+            v.counts[k2] = v2
+        smoothTotal += ( vocab_size - numNgrams)*cmap[0]
+        v.smoothTotal = smoothTotal
+        bigram.counts[k] = v
+
+    return (cmap, bigram)
+
+#
+# Computational Functions
+def findPerplexity(bigrams, unigrams, cmap, file_paths):
+    logsum = 0
+    divisor = 0
+    for path in file_paths:
+        # print ("Now reading %s", path)
+        raw = open(path, 'r').read()
+        processed_file = filePreprocessor(raw)
+        sen_tokens = sent_tokenize(processed_file)
+        for sen in sen_tokens:
+            # might need to point </s> to <s> for bigrams
+
+            sentence = stringPreprocessor(sen)
+            tokens = word_tokenize(sentence)
+            tokens.append("</s>")
+            for idx, token in enumerate(tokens):
+                tokenIdx_1 = "<s>"
+                if idx != 0:
+                    tokenIdx_1 = readTestToken(tokens[idx-1],unigrams)
+                token = readTestToken(token, unigrams)
+
+                # if bigrams.counts[tokenIdx_1].smoothTotal == 0:
+                    # print("uhg it's 0 ", bigrams.counts[tokenIdx_1].smoothTotal)
+                    # print("and tk-1 ", tokenIdx_1)
+                logsum -= math.log(readCmap(bigrams.counts[tokenIdx_1].counts[token].total, cmap)/bigrams.counts[tokenIdx_1].smoothTotal)
+                divisor += 1
+
+    return math.exp(1/divisor * logsum);
+
+
+def generateUnigramBigram():
+    category_paths =  glob.glob('data_corrected/classification_task/*')
+    unigramCollection = {}
+    bigramCollection = {}
+    # category_paths = [category_paths[0]]
+    for category in category_paths:
+        # BEGIN iteration over categories
+        print("---")
+        print("------------",category,"------------")
+        print("---")
+
+        unigrams = NGram(1)
+        bigrams = NGram(2)
+
+        file_paths = glob.glob(category+"/train_docs/*.txt")
+        # file_paths = [file_paths[0]]
+        # BEGIN iteration over paths
+        twothirdsmark = int(len(file_paths)*2/3)
+
+        training_file_paths = file_paths[:twothirdsmark]
+        test_file_paths = file_paths[twothirdsmark:]
+
+        unigrams = collectionIterator(training_file_paths,unigrams)
+        bigrams = collectionIterator(training_file_paths,bigrams,unigrams)
+
+        mappedUnigrams = removeUnk(unigrams)
+        # Now post process data for smoothing
+        cmap, mappedBigrams = smoothGoodTuring(bigrams)
+        perplexity = findPerplexity(mappedBigrams, mappedUnigrams, cmap, test_file_paths)
+        print ("and the perplexity is ", perplexity)
+
+        unigramCollection[category] = unigrams
+        bigramCollection[category] = bigrams
+
+        # print("-------- Unigram ----------")
+        # for i in range(15):
+        #     print(unigramGenSentence(unigrams, "<s>"))
+        #
+        # print("-------- Bigram ----------")
+        # for i in range(15):
+        #     print(bigramGenSentence(bigrams, "<s>"))
+
+    return (unigramCollection, bigramCollection)
+
+#
 # Raw Code
 #
 
@@ -80,44 +241,5 @@ def bigramGenSentence (bigrams, initial):
 # nltk.download('punkt')
 
 # Initialization
-category_paths =  glob.glob('data_corrected/classification_task/*')
 
-# category_paths = [category_paths[0]]
-for category in category_paths:
-    # BEGIN iteration over categories
-    print("---")
-    print("------------",category,"------------")
-    print("---")
-
-    unigrams = NGram(1)
-    bigrams = NGram(2)
-
-    file_paths = glob.glob(category+"/train_docs/*.txt")
-    # file_paths = [file_paths[0]]
-    # BEGIN iteration over paths
-    for path in file_paths:
-        # print ("Now reading %s", path)
-        raw = open(path, 'r').read()
-        processed_file = filePreprocessor(raw)
-        sen_tokens = sent_tokenize(processed_file)
-        for sen in sen_tokens:
-            # might want to add <s> token for beginning of sentence
-            sentence = stringPreprocessor(sen)
-            tokens = word_tokenize(sentence)
-            tokens.append("</s>")
-            for idx, token in enumerate(tokens):
-                tokenIdx_1 = "<s>"
-                if idx != 0:
-                    tokenIdx_1 = tokens[idx-1]
-
-                bigrams.add([tokenIdx_1,token])
-                unigrams.add([token])
-
-
-    print("-------- Unigram ----------")
-    for i in range(15):
-        print(unigramGenSentence(unigrams, "<s>"))
-
-    print("-------- Bigram ----------")
-    for i in range(15):
-        print(bigramGenSentence(bigrams, "<s>"))
+uCollect, biCollect = generateUnigramBigram();
